58adf9341fc42193114efd0db1c9cc1f5c7285be
François Leroux <francois.leroux.pro@gmail.com>
2024-02-20 15:30:27 +0100

    gitignore has been modified

---
+++ monCodeEnC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+   // printf() displays the string inside quotation
+   int a;
+   a = 1;
+   a++;
+   a = pow(a, 3);
+   printf("Hello, World! %d", a);
+   return 0;
+}
